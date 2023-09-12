25a4e80397e776c94a7ed6ce955157c36c64cb3a
Diego Faria <diegoafbrito12@gmail.com>
2023-09-12 13:59:44 -0400

    integrar generador de reportes

---
+++ reportes/pdfs.py
@@ -0,0 +1,169 @@
+from reportlab.platypus import Table, Image, Paragraph, TableStyle, Table, SimpleDocTemplate, Spacer
+from django.db.models import Sum
+from reportlab.lib.pagesizes import A4
+from io import BytesIO
+from reportlab.lib.styles import ParagraphStyle
+import datetime
+from django.http import HttpResponse
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+import numpy as np
+
+# Aquí irán los reportes en formato PDF
+
+prefijo = '' #Cambiar al hacer deployment en PythonAnywhere
+
+basicTableStyle = TableStyle(
+        [
+            ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
+        ])
+
+estiloMontos = TableStyle(
+        [
+            ('LINEABOVE', (0,1), (-1, 1), 1 ,colors.black),
+            ('LINEABOVE', (0,2), (-1,-1), 1 ,colors.black, None, (2,2)),
+            ('LINEBELOW', (0,2), (-1,-1), 1 ,colors.black, None, (2,2)),
+            ('LINEBELOW', (0,2), (-2,-2), 1 ,colors.black, None, (2,2)),
+        ])
+
+def generar_pdf(request,object_list):
+    def primera_pagina(canvas, doc):
+        width, height = A4
+        canvas.saveState()
+        titleStyle = ParagraphStyle(
+            'title',
+            fontSize=25,
+            fontFamily='Junge',
+            textTransform='uppercase'
+        )  
+
+        i = Image('static/img/logo.jpeg',width=80,height=80)
+        i.wrapOn(canvas,width,height)
+
+        if request.POST['tipo'] == 'estado_cuenta':
+            i.drawOn(canvas,100,480)
+        else:
+            i.drawOn(canvas,50,750)       
+
+        canvas.restoreState()
+
+    buff = BytesIO()
+    if request.POST['tipo'] == 'estado_cuenta':
+        doc = SimpleDocTemplate(buff,pagesize=A4[::-1], topMargin=30, bottomMargin=30)
+    else:
+        doc = SimpleDocTemplate(buff,pagesize=A4, topMargin=30, bottomMargin=30)
+    story = generar_historia(request, object_list)
+
+    doc.build(story, onFirstPage=primera_pagina)
+        
+    response = HttpResponse(content_type='application/pdf')
+
+    response.write(buff.getvalue())
+
+    if request.POST['tipo'] == 'recibo_pago':
+        newFile = open(prefijo + f"pdf/recibos/RECIBO_{object_list[0].num_rec}_{datetime.date.today().strftime('%Y_%m_%d')}.pdf", "wb")
+        newFileByteArray = bytearray(buff.getvalue())
+        newFile.write(newFileByteArray)
+
+    return response
+
+def generar_historia(request, object_list):
+    # Colocar los tipos de reporte de la siguiente forma:
+    if request.POST['tipo'] == 'estado_cuenta':
+        return estado_cuenta(request, object_list)
+
+def estado_cuenta(request, object_list):
+    import locale
+    locale.setlocale(locale.LC_TIME, "es_VE")
+    date = datetime.datetime.now().strftime('%a, %d/%m/%Y %H:%M:%S')
+
+    tabla = Table([[Paragraph("REPÚBLICA BOLIVARIANA DE VENEZUELA", ParagraphStyle('', alignment=1)), Paragraph(f'<b>FECHA DE EXPEDICIÓN:</b> {date}', ParagraphStyle('', alignment=2))],
+        [Paragraph("ALCALDÍA BOLIVARIANA DEL MUNICIPIO SUCRE", ParagraphStyle('', alignment=1)), Paragraph(f'<b>PATENTE: {object_list[0].cod_con.numpatente}</b>', ParagraphStyle('', alignment=2, textColor=colors.red)) if object_list[0].cod_con else ''],
+        [Paragraph("DIRECCIÓN DE ADMINISTRACIÓN TRIBUTARIA", ParagraphStyle('', alignment=1)), ''],
+        [Paragraph("G-20001595-0", ParagraphStyle('', alignment=1)), '']], colWidths=(6*inch, 2*inch), hAlign="RIGHT")
+
+    story = [tabla, Spacer(0, 20), Paragraph("<b>ESTADO DE CUENTA</b>", ParagraphStyle('', alignment=1, fontSize=15)), Spacer(0,10)]
+
+    if object_list[0].cod_con:
+        tabla_contr = Table([
+            [Paragraph("<b>RUBRO:</b>"), "PATENTE DE VEHÍCULOS"],
+            [Paragraph("<b>CÉDULA / RIF:</b>"), Paragraph(object_list[0].cod_con.ced_rif)],
+            [Paragraph("<b>NOMBRE:</b>"), Paragraph(object_list[0].cod_con.nom_con.upper())],
+            [Paragraph("<b> DIRECCIÓN:</b>"), Paragraph(object_list[0].cod_con.dir_cont.upper())],
+        ], colWidths=(2*inch, 7.5*inch), hAlign="CENTER")
+    else:
+        tabla_contr = Table([
+            [Paragraph("<b>RUBRO:</b>"), "PATENTE DE VEHÍCULOS"],
+            [Paragraph("<b>CÉDULA / RIF:</b>"), Paragraph(object_list[0].ced_rif)],
+            [Paragraph("<b>NOMBRE:</b>"), Paragraph(object_list[0].dueno().nombre_apellido.upper())],
+            [Paragraph("<b> DIRECCIÓN:</b>"), Paragraph(object_list[0].dueno().direccion.upper())],
+        ], colWidths=(2*inch, 7.5*inch), hAlign="CENTER")        
+
+    tabla_contr.setStyle(basicTableStyle)
+    story.append(tabla_contr)
+
+    total_deuda = 0
+
+    tdata = [[Paragraph("<b>DESCRIPCIÓN</b>"), Paragraph("<b>SIN DESCUENTO</b>"),
+            Paragraph("<b>DESCUENTO</b>"), Paragraph("<b>MONTO</b>")]]
+
+    for v in object_list:
+        total = 0
+        pendientes = Rcrecveh.objects.filter(cod_veh = v.cod_veh, descripcion__isnull = True, estutus = 0)
+        if pendientes.exists() and not v.exo_veh:
+            ano = pendientes.first().fec_rec.year - 1
+            for p in pendientes:
+                if (ano != p.fec_rec.year and not (p.fec_rec.year - 1 == ano and p.fec_rec.month == 1)):
+                    ano += 1
+
+                    if len(tdata) > 1 or 'descripcion' in locals():
+                        tdata.append([descripcion, str(total_local), "0.00", str(total_local)])
+                    
+                    descripcion = f"{v.pla_veh} {p.fec_rec.year} I " if p.fec_rec.month == 4 else f"{v.pla_veh} {p.fec_rec.year} II " if p.fec_rec.month == 7 else f"{v.pla_veh} {p.fec_rec.year} III "  if p.fec_rec.month == 10 else f"{v.pla_veh} {p.fec_rec.year-1} IV "
+                    total_local = 0
+                elif((p.fec_rec.year == ano + 1 and p.fec_rec.month == 1) and ('descripcion' in locals() and str(p.fec_rec.year-1) not in descripcion)):
+                    if len(tdata) > 1 or 'descripcion' in locals():
+                        tdata.append([descripcion, str(total_local), "0.00", str(total_local)])
+                    descripcion = f"{v.pla_veh} {p.fec_rec.year} I " if p.fec_rec.month == 4 else f"{v.pla_veh} {p.fec_rec.year} II " if p.fec_rec.month == 7 else f"{v.pla_veh} {p.fec_rec.year} III "  if p.fec_rec.month == 10 else f"{v.pla_veh} {p.fec_rec.year-1} IV "
+                    total_local = 0
+                else:
+                    try:
+                        print(2)
+                        descripcion += f"I " if p.fec_rec.month == 4 else f"II " if p.fec_rec.month == 7 else f"III "  if p.fec_rec.month == 10 else f"IV "
+                    except:
+                        print(3)
+                        descripcion = f"{v.pla_veh} {p.fec_rec.year} I " if p.fec_rec.month == 4 else f"{v.pla_veh} {p.fec_rec.year} II " if p.fec_rec.month == 7 else f"{v.pla_veh} {p.fec_rec.year} III "  if p.fec_rec.month == 10 else f"{v.pla_veh} {p.fec_rec.year-1} IV "
+                
+                if 'total_local' not in locals():
+                    total_local = 0
+
+                total_local += p.mon_pag
+                total += p.mon_pag
+            
+            total_deuda += total
+        elif v.exo_veh:
+            descripcion = f"{v.pla_veh} - EXONERADO"
+            total_local = 0
+    
+    try:
+        tdata.append([descripcion, str(total_local), "0.00", str(total_local)])
+    except:
+        print("Sin datos")
+    
+    tabla = Table(tdata, colWidths=(4*inch, 1.83*inch,1.83*inch,1.83*inch))
+    tabla.setStyle(estiloMontos)
+    story.append(tabla)
+
+    tabla = Table([
+        ['', Paragraph(f"<b>SUBTOTAL Bs:</b>"), Paragraph(f'Bs. {total_deuda}')],
+        [Paragraph(f"<b>ELABORADO POR:</b> {request.session['usuario']['nombre']}"), Paragraph("<b>TOTAL DESCUENTO:</b>"), Paragraph(f"{0.00}")],
+        [Paragraph(f"<b>VALOR DEL PETRO:</b> Bs. {CotizacionPetro.objects.last().valor}"),Paragraph("<b>TOTAL Bs:</b>"), Paragraph(f"<b>Bs. {total_deuda}</b>")],
+        [Paragraph("<b>VÁLIDO POR 5 DÍAS</b>"),Paragraph(f"<b>TOTAL Pt:</b>"), Paragraph(f"{round(total_deuda/CotizacionPetro.objects.last().valor, 6)}")]]
+    , colWidths=(5.83*inch,1.83*inch,1.83*inch), hAlign="RIGHT")
+
+    tabla.setStyle(TableStyle([
+        ('LINEABOVE', (0,0), (-1, 0), 1 ,colors.black),
+    ]))
+    story.append(tabla)
+
+    return story
