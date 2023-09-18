b43454bd438f23c6e2badd7a02e7b3e78c0419b3
Diego Faria <diegoafbrito12@gmail.com>
2023-09-18 12:38:13 -0400

    migrar datos de fluidos a la bdd

--- simulaciones_pequiven/views.py
+++ simulaciones_pequiven/views.py
@@ -1,7 +1,8 @@
 from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, login, get_user_model
-
+from django.http import HttpResponse
+from intercambiadores.models import Fluido, Unidades
 
 class Bienvenida(View):
     context = {
@@ -40,3 +41,23 @@ class CerrarSesion(View):
     def get(self, request):
         logout(request)
         return redirect('/')
+    
+class ComponerFluidos(View):
+    def get(self, request):
+        import csv
+        with open('./fluidos.csv','r') as file:
+            csvreader = csv.reader(file, delimiter=";")
+            for row in csvreader:
+                print(row)
+                Fluido.objects.create(
+                    nombre = row[0],
+                    formula = row[1],
+                    peso_molecular = row[2].replace(",","."),
+                    estado= row[3],
+                    unidad_temperatura = Unidades.objects.get(pk=1) if row[4] == 'C' else Unidades.objects.get(pk=2),
+                    a = float(row[5].replace(",",".")), b = float(row[6].replace(",",".")),
+                    c = float(row[7].replace(",",".")), d = float(row[8].replace(",",".")[:row[8].index('E')])*10**int(row[8][row[8].index('E')+1:]) if 'E' in row[8] else row[8].replace(',','.'),
+                    minimo = row[9] if len(row[9]) else -255, maximo = row[10] if len(row[10]) else 1000,
+                )
+
+            return HttpResponse("A")
