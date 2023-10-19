0191686164303a387057111b0000afa636e53f4b
Diego Faria <diegoafbrito12@gmail.com>
2023-10-18 21:13:14 -0400

    añadir & e:/Proyectos/sievep/sievep_indesca/sievep_indesca/env/Scripts/python.exe e:/Proyectos/sievep/sievep_indesca/sievep_indesca/calculos/termodinamicos.py

--- calculos/termodinamicos.py
+++ calculos/termodinamicos.py
@@ -1,6 +1,5 @@
-from thermo.heat_capacity import HeatCapacityGas, HeatCapacityLiquid, HeatCapacitySolid # CLASES
-import CoolProp.CoolProp as CP
 from thermo.chemical import Chemical
+from thermo.mixture import Mixture
 from .unidades import transformar_unidades_cp
 import numpy
 
@@ -20,40 +19,6 @@ def calcular_cp(fluido: str, t1: float, t2: float, unidad_salida = 29):
         float: Cp del fluido en esas condiciones 
     """
     t = numpy.mean([t1, t2])
-    quimico = Chemical(fluido)
-
-    mw = quimico.MW # PESO MOLECULAR
-
-    # Caso Especial del Aire
-    if(quimico.Tb == None):
-        return round(CP.PropsSI('C','P',101325,'T',t,'Air.mix'), 4)
-
-    if(t >= quimico.Tb): # Caso Gas
-        quimico = HeatCapacityGas(fluido)
-        for metodo in HeatCapacityGas.ranked_methods: # Búsqueda del método permitido
-            try:
-                cp = quimico.calculate(t, metodo)/mw*1000
-                break
-            except:
-                cp = 0
-                continue
-    elif(t <= quimico.Tm): # Caso Sólido
-        quimico = HeatCapacitySolid(fluido)
-        for metodo in HeatCapacitySolid.ranked_methods: # Búsqueda del método permitido
-            try:
-                cp = quimico.calculate(t, metodo)/mw*1000
-                break
-            except:
-                cp = 0
-                continue
-    else: # Caso Líquido
-        quimico = HeatCapacityLiquid(fluido)
-        for metodo in HeatCapacityLiquid.ranked_methods: # Búsqueda del método permitido
-            try:
-                cp = quimico.calculate(t, metodo)/mw*1000
-                break
-            except Exception as e:
-                cp = 0
-                continue        
+    cp = Chemical(fluido,T=t).Cp if fluido != '132259-10-0' else Mixture('air',T=t).Cp
 
     return round(transformar_unidades_cp([cp], 29, unidad_salida)[0], 4)
