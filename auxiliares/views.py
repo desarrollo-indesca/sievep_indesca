3883bdb6710811e8125a132eb214645565ec1abd
Diego Faria <diegoafbrito12@gmail.com>
2024-03-13 08:58:42 -0400

    añadir vista de consulta en las bombas

--- auxiliares/views.py
+++ auxiliares/views.py
@@ -1,6 +1,13 @@
+from typing import Any
+
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
+from django.views.generic import ListView
+from django.http import HttpRequest, HttpResponse
+
+from auxiliares.models import *
+from intercambiadores.models import Complejo, Planta
 
 # Create your views here.
 
@@ -23,3 +30,93 @@ class SeleccionEquipo(LoginRequiredMixin, View):
 
     def get(self, request):
         return render(request, 'seleccion_equipo.html', context=self.context)
+
+# VISTA DE BOMBAS
+
+class ConsultaBombas(LoginRequiredMixin, ListView):
+    """
+    Resumen:
+        Vista para la consulta general de las bombas centrífugas (primer equipo auxiliar)
+
+    Atributos:
+        context: dict
+            Contexto de la vista. Actualmente solo incluye el título predeterminado.
+    
+    Métodos:
+        get(self, request)
+            Renderiza la plantilla de selección de equipos.
+    """
+
+    model = Bombas
+    template_name = 'bombas/consulta.html'
+    paginate_by = 10
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if(request.GET.get('page')):
+            request.session['pagina_consulta'] = request.GET['page']
+        else:
+            request.session['pagina_consulta'] = 1
+        
+        request.session['tag_consulta'] = request.GET.get('tag') if request.GET.get('tag') else ''
+        request.session['descripcion_consulta'] = request.GET.get('descripcion') if request.GET.get('descripcion') else ''
+        request.session['complejo_consulta'] = request.GET.get('complejo') if request.GET.get('complejo') else ''
+        request.session['planta_consulta'] = request.GET.get('planta') if request.GET.get('planta') else ''
+        
+        return super().get(request, *args, **kwargs)
+    
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo"] = "SIEVEP - Consulta de Bombas Centrífugas"
+        context['complejos'] = Complejo.objects.all()
+
+        if(self.request.GET.get('complejo')):
+            context['plantas'] = Planta.objects.filter(complejo= self.request.GET.get('complejo'))
+
+        context['tag'] = self.request.GET.get('tag', '')
+        context['descripcion'] = self.request.GET.get('descripcion', '')
+        context['complejox'] = self.request.GET.get('complejo')
+        context['plantax'] = self.request.GET.get('planta')
+
+        if(context['complejox']):
+            context['complejox'] = int(context['complejox'])
+        
+        if(context['plantax']):
+            context['plantax'] = int(context['plantax'])
+
+        # context['link_creacion'] = 'crear_bomba'
+
+        return context
+    
+    def get_queryset(self):
+        tag = self.request.GET.get('tag', '')
+        descripcion = self.request.GET.get('descripcion', '')
+        complejo = self.request.GET.get('complejo', '')
+        planta = self.request.GET.get('planta', '')
+
+        new_context = None
+
+        if(planta != '' and complejo != ''):
+            new_context = self.model.objects.filter(
+                planta__pk=planta
+            )
+        elif(complejo != ''):
+            new_context = new_context.filter(
+                planta__complejo__pk=complejo
+            ) if new_context else self.model.objects.filter(
+                planta__complejo__pk=complejo
+            )
+
+        if(not(new_context is None)):
+            new_context = new_context.filter(
+                descripcion__icontains = descripcion,
+                tag__icontains = tag
+            )
+        else:
+            new_context = self.model.objects.filter(
+                descripcion__icontains = descripcion,
+                tag__icontains = tag
+            )
+
+        return new_context
+
