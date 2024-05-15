ac3d113172891e0a8e0a660c8458eeb544c548c9
Diego Faria <diegoafbrito12@gmail.com>
2024-05-15 10:14:18 -0400

    añadir comentarios a modelos de turbinas documentación

--- turbinas/models.py
+++ turbinas/models.py
@@ -17,6 +17,16 @@ FASES_CORRIENTES = (
 )
 
 class DatosCorrientes(models.Model):
+    """
+    Resumen:
+        Modelo de datos de datos asociados a TODAS las corrientes circulantes por turbinas de vapor.
+
+    Atributos:
+        flujo_unidad: Unidad (F,K) -> Unidad de flujo másico o volumétrico para los flujos circulantes en unidades.
+        entalpia_unidad: Unidad (n) -> Unidad de entalpía (másica) de la corriente
+        presion_unidad: Unidad (P) -> Unidad de presión (g) bajo la que está la corriente
+        temperatura_unidad: Unidad (T) -> Unidad de temperatura a la que está la corriente  
+    """
     flujo_unidad = models.ForeignKey(Unidades, on_delete=models.PROTECT, related_name="flujo_unidad_corriente")
     entalpia_unidad = models.ForeignKey(Unidades, on_delete=models.PROTECT, related_name="entalpia_unidad_corriente")
     presion_unidad = models.ForeignKey(Unidades, on_delete=models.PROTECT, related_name="presion_unidad_corriente")
@@ -26,6 +36,21 @@ class DatosCorrientes(models.Model):
         db_table = "turbinas_vapor_datoscorrientes"
 
 class Corriente(models.Model):
+    """
+    Resumen:
+        Modelo de datos de las corrientes circulantes por turbinas de vapor.
+
+    Atributos:
+       numero_corriente: CharField(10) -> Número de la corriente de la turbina.
+       descripcion_corriente: CharField(50) -> Descripción la corriente.
+       flujo: FloatField -> Flujo circulante
+       entalpia: FloatField -> Entalpía de la corriente
+       presion: FloatField ->  Presión bajo la que está la corriente / Si es salida no se requiere
+       temperatura: FloatField -> Temperatura bajo la que está la corriente
+       fase: CharField(1) -> Fase en la que se encuentra la corriente
+       entrada: BooleanField -> Es entrada / No es enntrada
+       datos_corriente: DatosCorrientes -> Datos asociados con la corriente
+    """
     numero_corriente = models.CharField('Número Corriente', max_length=10)
     descripcion_corriente = models.CharField('Descripción de la Corriente', max_length=50)
 
@@ -45,6 +70,26 @@ class Corriente(models.Model):
         ordering = ('-entrada', 'numero_corriente')
 
 class GeneradorElectrico(models.Model):
+    """
+    Resumen:
+        Modelo de datos del generador eléctrico de las turbinas de vapor.
+
+    Atributos:
+        polos: PositiveSmallIntegerField -> Polos del generador
+        fases: PositiveSmallIntegerField -> Fases del generador
+        ciclos: FloatField -> Ciclos del generador
+        ciclos_unidad: Unidad (H) -> Unidad de frecuencia de los ciclos 
+        potencia_real: FloatField -> Potencia real del generador
+        potencia_real_unidad: Unidad (B) -> Unidad de potencia
+        potencia_aparente: FloatField -> Potencia aparente del generador 
+        potencia_aparente_unidad: Unidad (Z) -> Unidad de potencia aparente del generador
+        velocidad: FloatField -> Velocidad del generador
+        velocidad_unidad: Unidad (O) -> Unidad de velocidad angular para la velocidad del generador
+        corriente_electrica: FloatField -> Cantidad de corriente generada
+        corriente_electrica_unidad: Unidad (Y) -> Unidad de corriente eléctrica generada 
+        voltaje: FloatField -> Voltaje del generador
+        voltaje_unidad: Unidad (X) -> Unidad de voltaje del generador
+    """
     polos = models.PositiveSmallIntegerField()
     fases = models.PositiveSmallIntegerField()
     
@@ -70,6 +115,24 @@ class GeneradorElectrico(models.Model):
         db_table = "turbinas_vapor_generadorelectrico"
 
 class EspecificacionesTurbinaVapor(models.Model):
+    """
+    Resumen:
+        Modelo de datos de las especificaciones técnicas de las turbinas de vapor.
+
+    Atributos:
+        potencia: FloatField -> Potencia de la turbina de vapor
+        potencia_max: FloatField -> Potencia máxima de la turbina de vapor
+        potencia_unidad: Unidad (B) -> Unidad de la potencia 
+        velocidad: FloatField -> Velocidad de la turbina de vapor
+        velocidad_unidad: Unidad (O) -> Unidad de velocidad angular de la turbina de vapor 
+        presion_entrada: FloatField -> Presión de entrada de la turbina de vapor
+        presion_entrada_unidad: Unidad (P) -> Unidad de la presión de entrada a la turbina 
+        temperatura_entrada: FloatField -> Temperatura de entrada a la turbina
+        temperatura_entrada_unidad: Unidad (T) ->  Unidad de la temperatura de entrada a la turbina
+        contra_presion: FloatField -> Contrapresión de la turbina de vapor
+        contra_presion_unidad: Unidad (P) -> Unidad de presión de la contrapresión 
+        eficiencia: FloatField -> Eficiencia calculada de la turbina al ser registrada en el sistema. A veces no se puede calcular.
+    """
     potencia = models.FloatField(validators=[MinValueValidator(0.000001)])
     potencia_max = models.FloatField('Potencia Máxima', validators=[MinValueValidator(0.000001)])
     potencia_unidad = models.ForeignKey(Unidades, on_delete=models.PROTECT, related_name="potencia_unidad_turbinavapor")
@@ -92,6 +155,24 @@ class EspecificacionesTurbinaVapor(models.Model):
         db_table = "turbinas_vapor_gespecificaciones"
 
 class TurbinaVapor(models.Model):
+    """
+    Resumen:
+        Modelo de datos de las turbinas de vapor. Modelo central.
+
+    Atributos:
+        tag: CharField(45) -> Tag único de la turbina de vapor
+        descripcion: CharField(100) -> Descripción de la turbina de vapor (funciones que cumple etc)
+        fabricante: CharField(45) -> Fabricante de la turbina de vapor
+        modelo: CharField(45) -> Modelo de la turbina (opcional)
+        planta: Planta -> Planta donde se encuentra
+        generador_electrico: GeneradorElectrico -> Generador Eléctrico de la turbina
+        especificaciones: EspecificacionesTurbinaVapor -> Especificaciones de la turbina
+        datos_corrientes: DatosCorrientes -> Datos generales de las corrientes que pasan por la turbina
+        creado_por: Usuario -> Turbina creada por
+        editado_por: Usuario -> Turbina editada por última vez por
+        creado_al: DateTimeField -> Fecha de creación
+        editado_al: DateTimeField -> Fecha de última edición
+    """
     # Identificación de la turbina
     tag = models.CharField(max_length=10, unique=True)
     descripcion = models.CharField('Descripción', max_length=100)
@@ -117,6 +198,19 @@ class TurbinaVapor(models.Model):
 ## Modelos de Evaluación
 
 class EntradaEvaluacion(models.Model):
+    """
+    Resumen:
+        Modelo de datos de la entrada de una evaluación de turbinas de vapor.
+
+    Atributos:
+        id: UUIDField -> ID único del objeto
+        flujo_entrada: FloatField -> Flujo de entrada de la evaluación
+        flujo_entrada_unidad: Unidad (F,K) -> Unidad del flujo de entrada y los flujos calculados de las corrientes
+        potencia_real: FloatField -> Potencia real ingresada
+        potencia_real_unidad: Unidad (B) -> Unidad de la potencia real
+        presion_unidad: FloatField -> Unidad de la presión ingresada
+        temperatura_unidad: Unidad (T) -> Unidad de la temperatura ingresada
+    """
     id = models.UUIDField(primary_key=True, default = uuid.uuid4)
     flujo_entrada = models.FloatField(validators=[MinValueValidator(0.00001)])
     flujo_entrada_unidad = models.ForeignKey(Unidades, on_delete=models.PROTECT, related_name="flujo_entrada_unidad_entradaevaluacion")
@@ -131,6 +225,16 @@ class EntradaEvaluacion(models.Model):
         db_table = "turbinas_vapor_evaluacion_entrada"
 
 class SalidaEvaluacion(models.Model):
+    """
+    Resumen:
+        Modelo de datos de la salida de una evaluación de turbinas de vapor.
+
+    Atributos:
+        id: UUIDField -> UUID del objeto
+        eficiencia: FloatField -> Eficiencia calculada en la evaluación
+        potencia_calculada: FloatField -> Potencial calculada en la evaluación
+        entalpia_unidad: Unidad (n) -> Unidad de la entalpía dada como salida en las corrientes. Tomará la que se encuentre en el diseño.
+    """
     id = models.UUIDField(primary_key=True, default = uuid.uuid4)
     eficiencia = models.FloatField()
     potencia_calculada = models.FloatField()
@@ -140,6 +244,16 @@ class SalidaEvaluacion(models.Model):
         db_table = "turbinas_vapor_evaluacion_salida"
 
 class EntradaCorriente(models.Model):
+    """
+    Resumen:
+        Modelo de datos de los datos de entrada de una corriente en una evaluación de turbinas de vapor.
+
+    Atributos:
+        id: UUIDField -> UUID del objeto
+        eficiencia: FloatField -> Eficiencia calculada en la evaluación
+        presion: Floatfield -> Presión de la corriente
+        temperatura: Floatfield -> Temperatura de la corriente
+    """
     id = models.UUIDField(primary_key=True, default = uuid.uuid4)
     presion = models.FloatField(validators=[MinValueValidator(0)], null=True, blank=True)
     temperatura = models.FloatField(validators=[MinValueValidator(-273.15)])
@@ -148,6 +262,16 @@ class EntradaCorriente(models.Model):
         db_table = "turbinas_vapor_evaluacion_entradacorriente"
 
 class SalidaCorriente(models.Model):
+    """
+    Resumen:
+        Modelo de datos de los datos de salida de una corriente en una evaluación de turbinas de vapor.
+
+    Atributos:
+        id: UUIDField -> UUID del objeto
+        flujo: FloatField -> Flujo calculado en la evaluación
+        entalpia: Floatfield -> Entalpía calculada de la corriente
+        fase: CharField(1) -> Fase de la corriente
+    """
     id = models.UUIDField(primary_key=True, default = uuid.uuid4)
     flujo = models.FloatField()
     entalpia = models.FloatField()
@@ -160,6 +284,20 @@ class SalidaCorriente(models.Model):
         db_table = "turbinas_vapor_evaluacion_salidacorriente"
 
 class Evaluacion(models.Model):
+    """
+    Resumen:
+        Modelo de datos de una evaluación de turbinas de vapor.
+
+    Atributos:
+        id: UUIDField -> UUID del objeto
+        equipo: TurbinaVapor -> Turbina de Vapor a la cual fue realizada la evaluación
+        creado_por: Usuario -> Usuario que registró la evaluación
+        nombre: CharField(45) -> Nombre asignado a la evaluación
+        fecha: DateTimeField -> Fecha de la evaluación (automáticamente añadida) 
+        activo: BooleanField -> Indica si la evaluación se encuentra activa o no.
+        entrada: 1:1 EntradaEvaluacion -> Datos de entrada de la evaluación.
+        salida: 1:1 SalidaEvaluacion -> Datos de salida de la evaluación.
+    """
     id = models.UUIDField(primary_key=True, default = uuid.uuid4)
     equipo = models.ForeignKey(TurbinaVapor, on_delete=models.PROTECT)
     creado_por = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
@@ -176,6 +314,18 @@ class Evaluacion(models.Model):
         db_table = "turbinas_vapor_evaluacion"
 
 class CorrienteEvaluacion(models.Model):
+    """
+    Resumen:
+        Modelo de datos para cada corriente circulantes por la turbina de vapor evaluada.
+
+    Atributos:
+        id: UUIDField -> UUID del objeto
+        corriente: Corriente -> Corriente asociada 
+        entrada: EntradaCorriente -> Datos de Entrada de la corriente 
+        salida: Evaluacion -> Datos de Salida de la corriente
+        evaluacion: Evaluacion -> Datos de la evaluación
+    """
+    id = models.UUIDField(primary_key=True, default = uuid.uuid4)
     corriente = models.ForeignKey(Corriente, on_delete=models.PROTECT)
     entrada = models.OneToOneField(EntradaCorriente, on_delete=models.PROTECT)
     salida = models.OneToOneField(SalidaCorriente, on_delete=models.PROTECT)
