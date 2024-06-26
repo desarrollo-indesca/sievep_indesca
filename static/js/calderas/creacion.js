f1a12f37391b533feb297dfd917379c81e653399
Diego Faria <diegoafbrito12@gmail.com>
2024-06-26 14:45:28 -0400

    continuar con js

---
+++ static/js/calderas/creacion.js
@@ -0,0 +1,27 @@
+$("#id_especificaciones-caldera-temperatura_unidad").change(() => {
+    $("select[name='especificaciones-caldera-temperatura_unidad']").val($("#id_especificaciones-caldera-temperatura_unidad").val())
+});
+
+$("#id_especificaciones-caldera-presion_unidad").change(() => {
+    $("select[name='especificaciones-caldera-presion_unidad']").val($("#id_especificaciones-caldera-presion_unidad").val())
+});
+
+$("#id_tambor-temperatura_unidad").change(() => {
+    $("select[name='tambor-temperatura_unidad']").val($("#id_tambor-temperatura_unidad").val())
+});
+
+$("#id_sobrecalentador-presion_unidad").change(() => {
+    $("select[name='sobrecalentador-presion_unidad']").val($("#id_sobrecalentador-presion_unidad").val())
+});
+
+$("#id_sobrecalentador-presion_unidad").change(() => {
+    $("select[name='sobrecalentador-presion_unidad']").val($("#id_sobrecalentador-presion_unidad").val())
+});
+
+$("#id_dimensiones-caldera-dimensiones_unidad").change(() => {
+    $("select[name='dimensiones-caldera-dimensiones_unidad']").val($("#id_dimensiones-caldera-dimensiones_unidad").val())
+});
+
+$("#id_tambor-presion_unidad").change(() => {
+    $("select[name='tambor-presion_unidad']").val($("#id_tambor-presion_unidad").val())
+});
