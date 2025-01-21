449044c48f8d535abc884e714f4d404c1765c022
Diego Faria <diegoafbrito12@gmail.com>
2025-01-21 14:40:42 -0400

    cambiar status

--- simulaciones_pequiven/settings.py
+++ simulaciones_pequiven/settings.py
@@ -59,7 +59,7 @@ with open(os.path.join(BASE_DIR, 'secret-key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 LOGIN_URL = '/'
