c9e3bac402a1e48fbbe72d26bfca02c54394a7fa
Diego Faria <diegoafbrito12@gmail.com>
2025-02-11 15:45:24 -0400

    avanzar en duplicación

--- compresores/views.py
+++ compresores/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from django.db.models import Prefetch
-from simulaciones_pequiven.views import FiltradoSimpleMixin
+from simulaciones_pequiven.views import FiltradoSimpleMixin, DuplicateView
 from .models import *
 from reportes.pdfs import generar_pdf
 from reportes.xlsx import reporte_equipos
@@ -25,6 +25,34 @@ class CargarCompresorMixin():
         if(prefetch):
             compresor = compresor.select_related(
                 'creado_por', 'editado_por', 'planta'
+            ).prefetch_related(
+                Prefetch(
+                    'casos', PropiedadesCompresor.objects.select_related(
+                        'tipo_lubricacion', 
+                        'unidad_potencia', 
+                        'unidad_velocidad'
+                    ).prefetch_related(
+                        Prefetch(
+                            'etapas',
+                            EtapaCompresor.objects.select_related(
+                                'flujo_masico_unidad',
+                                'flujo_molar_unidad',
+                                'densidad_unidad',
+                                'potencia_unidad',
+                                'cabezal_unidad',
+                                'volumen_unidad'
+                            ).prefetch_related(
+                                Prefetch(
+                                    'lados',
+                                    LadoEtapaCompresor.objects.select_related(
+                                        'temp_unidad',
+                                        'presion_unidad'
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
             )
         
         if(not queryset):
@@ -109,3 +137,59 @@ class ConsultaCompresores(FiltradoSimpleMixin, ReportesFichasCompresoresMixin, C
         new_context = self.get_compresor(True, self.filtrar_equipos())
 
         return new_context
+
+
+class DuplicarCaldera(CargarCompresorMixin, DuplicateView):
+    """
+    Resumen:
+        Vista para crear una copia temporal duplicada de una caldera para hacer pruebas en los equipos.
+    """
+
+    def post(self, request, pk):
+        compresor_original = Compresor.objects.select_related(
+            "sobrecalentador", "sobrecalentador__dims", "tambor",
+            "dimensiones", "especificaciones", "combustible", 
+            "chimenea", "economizador"
+        ).prefetch_related(
+            "tambor__secciones_tambor", "combustible__composicion_combustible_caldera",
+            "caracteristicas_caldera", "corrientes_caldera"
+        ).get(pk=pk)
+
+        if(self.request.user.is_superuser or PlantaAccesible.objects.filter(usuario = request.user, planta = caldera_original.planta, duplicacion = True).exists()):
+            caldera = caldera_original
+            caldera.copia = True
+            caldera.tag = generate_nonexistent_tag(Caldera, caldera.tag)
+            dims = self.copy(caldera_original.sobrecalentador.dims)
+            sobrecalentador = caldera_original.sobrecalentador
+            sobrecalentador.dims = dims
+            caldera.sobrecalentador = self.copy(caldera_original.sobrecalentador)
+            caldera.tambor = self.copy(caldera_original.tambor)
+            caldera.dimensiones = self.copy(caldera_original.dimensiones)
+            caldera.especificaciones = self.copy(caldera_original.especificaciones)
+            caldera.chimenea = self.copy(caldera_original.chimenea)
+            caldera.economizador = self.copy(caldera_original.economizador)
+            caldera.combustible = self.copy(caldera_original.combustible)
+            caldera.descripcion = f"COPIA DE LA CALDERA {caldera_original.tag}"
+            caldera = self.copy(caldera)
+
+            for caracteristica in caldera_original.caracteristicas_caldera.all():
+                caracteristica.caldera = caldera
+                self.copy(caracteristica)
+
+            for seccion in caldera_original.tambor.secciones_tambor.all():
+                seccion.tambor = caldera.tambor
+                self.copy(seccion)
+
+            for corriente in caldera_original.corrientes_caldera.all():
+                corriente.caldera = caldera
+                self.copy(corriente)
+
+            for compuesto in caldera_original.combustible.composicion_combustible_caldera.all():
+                compuesto.combustible = caldera.combustible
+                self.copy(compuesto)
+
+            caldera_original = Caldera.objects.get(pk=pk)
+            messages.success(request, f"Se ha creado la copia de la caldera {caldera_original.tag} como {caldera.tag}. Recuerde que todas las copias serán eliminadas junto a sus datos asociados al día siguiente a las 7:00am.")
+            return redirect("/calderas")
+        else:
+            return HttpResponseForbidden()
